8765c9a4e3b8ffeb4963b8b821fb544b68aa0787
danielduron <daniel.salinas@msu.montana.edu>
2019-07-09 19:05:59 -0600

    add pixel clustering

--- src/clusterPixels.py
+++ src/clusterPixels.py
@@ -1,17 +1,27 @@
 from applyConvolution import getImages
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+import scipy
+import numpy
 
 def kmeansImage(image):
 	# pixel per row
 	pixels = image.reshape( (-1,image.shape[-1]) )
-	kmeans = KMeans(n_clusters=4)
-	kmeans.fit(pixels)
+	kmeans = \
+	KMeans(n_clusters=3,max_iter=30,algorithm="elkan",n_jobs=4,n_init=5)
+	labels = kmeans.fit_predict(pixels).reshape(image.shape[0:2])
+	result = numpy.zeros(image.shape,dtype=image.dtype)
+	color = kmeans.cluster_centers_[labels[0,0]]
+	for r in range(labels.shape[0]) :
+		for c in range(labels.shape[1]) :
+			color = kmeans.cluster_centers_[labels[r,c]]
+			result[r,c,:] = color
+	return result
 
 if __name__ == "__main__" :
 	from sys import argv
 	if len(argv) >  1 :
 		with getImages( argv[1:] ) as ( images, opened, failed ) :
 			for i in images :
-				kmeansImage(i)
+				 plt.imshow(kmeansImage(i))
+				 plt.show()
